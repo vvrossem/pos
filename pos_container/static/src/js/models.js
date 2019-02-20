0f1a3d12e1547b86773389d8863f960ece6406fc
robin.keunen <robin@keunen.net>
2019-02-20 17:03:21 +0100

    [ADD] pos_container: add container list - broken

---
+++ pos_container/static/src/js/models.js
@@ -0,0 +1,25 @@
+/*
+    Copyright 2019 Coop IT Easy SCRLfs
+    	    Robin Keunen <robin@coopiteasy.be>
+    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+*/
+
+
+odoo.define('pos_container.pos_container', function (require) {
+    "use strict";
+
+
+    var models = require('point_of_sale.models');
+    var core = require('web.core');
+
+    // example: add field from defined model
+    // models.load_fields('res.partner','loyalty_points');
+
+    models.load_models([
+        {
+            model: 'pos.container',
+            fields: ['name','ean13', 'weight'],
+            // domain: function(self){ return [['id','=',self.config.loyalty_id[0]]]; },
+        }]
+    )
+});
