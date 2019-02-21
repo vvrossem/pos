45c11b586087e46b424bbf2a3a5f3a0c77b93996
Pierrick Brun <pierrick.brun@akretion.com>
2019-02-21 15:49:07 +0100

    ADD new barcode rule type

---
+++ pos_container/models/barcode.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Coop IT Easy SCRLfs
+# 	    Pierrick Brun <pierrick.brun@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class BarcodeRule(models.Model):
+    _inherit = 'barcode.rule'
+
+    type = fields.Selection(
+        selection_add = [('container', 'Container unit')],
+    )
