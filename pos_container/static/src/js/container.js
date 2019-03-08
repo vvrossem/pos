b511138a4caf1f0e2972faa2cff4d05ce697c937
Pierrick Brun <pierrick.brun@akretion.com>
2019-03-08 10:17:42 +0100

    [FIX] tabs instead of spaces

--- pos_container/static/src/js/container.js
+++ pos_container/static/src/js/container.js
@@ -1,7 +1,7 @@
 /*
     Copyright 2019 Coop IT Easy SCRLfs
-    	    Robin Keunen <robin@coopiteasy.be>
-    	    Pierrick Brun <pierrick.brun@akretion.com>
+            Robin Keunen <robin@coopiteasy.be>
+            Pierrick Brun <pierrick.brun@akretion.com>
     License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 */
 
