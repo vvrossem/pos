cf934972571b3735cd95e17be0bbe1b0472f12a2
Vincent Van Rossem <vvrossem@gmail.com>
2019-03-11 16:51:45 +0100

    [REF] removed unused code

--- pos_toledo_product/__manifest__.py
+++ pos_toledo_product/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2019 Coop IT Easy SCRLfs
-# 	    Robin Keunen <robin@coopiteasy.be>
+#       Robin Keunen <robin@coopiteasy.be>
 #       Vincent Van Rossem <vvrossem@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
