9eb4ba545aa2d9d5df7c750e610d97c4ecf34845
Vincent Van Rossem <vvrossem@gmail.com>
2019-04-02 11:44:17 +0200

    [ADD] new module: hw_customer_display_currency

---
+++ hw_customer_display_currency/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Coop IT Easy SCRLfs
+# 	    Vincent Van Rossem <vvrossem@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).{
+{
+    'name': "Currency Management Extension for the Hardware Customer Display ",
+    'version': '12.0.1.0.0',
+    'summary': """
+        Adds currency management to the Hardware Customer Display""",
+
+    "author": "Coop IT Easy SCRLfs, "
+              "Odoo Community Association (OCA)",
+    'license': "AGPL-3",
+    'website': "https://github.com/OCA/pos/",
+    'category': 'Hardware Drivers',
+    'depends': ['hw_customer_display'],
+    'data': [],
+    'installable': False,
+}
