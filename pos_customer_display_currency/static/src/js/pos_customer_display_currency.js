bea9f00ad3c9be0cb5afe639fcda50f66b1e138d
Vincent Van Rossem <vvrossem@gmail.com>
2019-04-03 17:19:53 +0200

    [WIP] .js: add bulk logic

--- pos_customer_display_currency/static/src/js/pos_customer_display_currency.js
+++ pos_customer_display_currency/static/src/js/pos_customer_display_currency.js
@@ -8,94 +8,82 @@ odoo.define('pos_customer_display_currency.pos_customer_display_currency', funct
     "use strict";
     var chrome = require('point_of_sale.chrome');
     var core = require('web.core');
+    var utils = require('web.utils')
     var devices = require('point_of_sale.devices');
     var gui = require('point_of_sale.gui');
     var models = require('point_of_sale.models');
     var screens = require('point_of_sale.screens');
     var _t = core._t;
 
+    var round_pr = utils.round_precision;
+
+
     models.PosModel = models.PosModel.extend({
 
         prepare_text_customer_display: function (type, data) {
-            if (this.config.iface_customer_display != true)
+            //console.log(this);
+            if (!this.config.iface_customer_display)
                 return;
+
             var line_length = this.config.customer_display_line_length || 20;
             var currency_rounding = this.currency.decimals;
             var currency_char = this.config.customer_display_currency_char;
+            var lines_to_send = [];
+            var line = {};
 
-            if (type == 'add_update_line') {
-                var line = data['line'];
-                var price_unit = line.get_unit_price();
-                var discount = line.get_discount();
-                if (discount) {
-                    price_unit = price_unit * (1.0 - (discount / 100.0));
-                }
-                price_unit = price_unit.toFixed(currency_rounding);
-                var qty = line.get_quantity();
-                // only display decimals when qty is not an integer
-                if (qty.toFixed(0) == qty) {
-                    qty = qty.toFixed(0);
-                }
-                // only display unit when != Unit(s)
-                var unit = line.get_unit();
-                var unit_display = '';
-                if (unit && !unit.is_unit) {
-                    unit_display = unit.name;
-                }
-                var l21 = qty + unit_display + ' x ' + price_unit;
+            if (type === 'add_update_line') {
+                console.log('add_update_line');
+                line = data['line'];
+                var price_unit = (line.get_unit_price() * (1.0 - (line.get_discount() / 100.0)))
+                                    .toFixed(currency_rounding);
+
+                var l21 = line.get_quantity_str_with_unit() + ' x ' + line.get_unit_price_with_unit(currency_char);
                 var l22 = ' ' + line.get_display_price().toFixed(currency_rounding) + currency_char;
-                var lines_to_send = new Array(
-                    this.proxy.align_left(line.get_product().display_name, line_length),
-                    this.proxy.align_left(l21, line_length - l22.length) + l22
-                );
+                lines_to_send = [
+                    this.proxy.align_left(line.get_product().display_name, line_length - l22.length) + l22,
+                    this.proxy.align_left(l21, line_length)
+                    ];
 
-            } else if (type == 'remove_orderline') {
+            } else if (type === 'remove_orderline') {
+                console.log('remove_orderline');
                 // first click on the backspace button set the amount to 0 => we can't precise the deleted qunatity and price
-                var line = data['line'];
-                var lines_to_send = new Array(
-                    this.proxy.align_left(_t("Delete Item"), line_length),
-                    this.proxy.align_right(line.get_product().display_name, line_length)
-                );
+                line = data['line'];
+                lines_to_send = [this.proxy.align_left(_t("Delete Item"), line_length),
+                    this.proxy.align_right(line.get_product().display_name, line_length)];
 
-            } else if (type == 'add_paymentline') {
-                var total = this.get('selectedOrder').get_total_with_tax().toFixed(currency_rounding);
-                var lines_to_send = new Array(
-                    this.proxy.align_left(_t("TOTAL: "), line_length),
-                    this.proxy.align_right(total + currency_char, line_length)
-                );
+            } else if (type === 'add_paymentline') {
+                console.log('add_paymentline');
+                var total = this.get('selectedOrder').get_total_with_tax().toFixed(currency_rounding) + currency_char;
+                lines_to_send = [this.proxy.align_left(_t("TOTAL: "), line_length),
+                    this.proxy.align_right(total, line_length)];
 
-            } else if (type == 'remove_paymentline') {
-                var line = data['line'];
-                var amount = line.get_amount().toFixed(currency_rounding);
-                var lines_to_send = new Array(
-                    this.proxy.align_left(_t("Cancel Payment"), line_length),
-                    this.proxy.align_right(line.cashregister.journal_id[1], line_length - 1 - amount.length) + ' ' + amount
-                );
+            } else if (type === 'remove_paymentline') {
+                console.log('remove_paymentline');
+                line = data['line'];
+                var amount = line.get_amount().toFixed(currency_rounding) + currency_char;
+                lines_to_send = [this.proxy.align_left(_t("Cancel Payment"), line_length),
+                    this.proxy.align_right(line.cashregister.journal_id[1], line_length - 1 - amount.length) + ' ' + amount];
 
-            } else if (type == 'update_payment') {
-                var change = data['change'];
-                var lines_to_send = new Array(
-                    this.proxy.align_left(_t("Your Change:"), line_length),
-                    this.proxy.align_right(change, line_length)
-                );
+            } else if (type === 'update_payment') {
+                console.log('update_payment');
+                var change = data['change'] + currency_char;
+                lines_to_send = [this.proxy.align_left(_t("Your Change:"), line_length),
+                    this.proxy.align_right(change, line_length)];
 
-            } else if (type == 'push_order') {
-                var lines_to_send = new Array(
-                    this.proxy.align_center(this.config.customer_display_msg_next_l1, line_length),
-                    this.proxy.align_center(this.config.customer_display_msg_next_l2, line_length)
-                );
+            } else if (type === 'push_order') {
+                console.log('push_order');
+                lines_to_send = [this.proxy.align_center(this.config.customer_display_msg_next_l1, line_length),
+                    this.proxy.align_center(this.config.customer_display_msg_next_l2, line_length)];
 
-            } else if (type == 'openPOS') {
-                var lines_to_send = new Array(
-                    this.proxy.align_center(this.config.customer_display_msg_next_l1, line_length),
-                    this.proxy.align_center(this.config.customer_display_msg_next_l2, line_length)
-                );
+            } else if (type === 'openPOS') {
+                console.log('openPOS');
+                lines_to_send = [this.proxy.align_center(this.config.customer_display_msg_next_l1, line_length),
+                    this.proxy.align_center(this.config.customer_display_msg_next_l2, line_length)];
 
-            } else if (type = 'closePOS') {
-                var lines_to_send = new Array(
-                    this.proxy.align_center(this.config.customer_display_msg_closed_l1, line_length),
-                    this.proxy.align_center(this.config.customer_display_msg_closed_l2, line_length)
-                );
+            } else if (type === 'closePOS') {
+                console.log('closePOS');
+                lines_to_send = [this.proxy.align_center(this.config.customer_display_msg_closed_l1, line_length),
+                    this.proxy.align_center(this.config.customer_display_msg_closed_l2, line_length)];
             } else {
                 console.warn('Unknown message type');
                 return;
@@ -109,17 +97,40 @@ odoo.define('pos_customer_display_currency.pos_customer_display_currency', funct
             var currency_data = {
                 'currency_code': this.currency.name,
                 'currency_char': this.config.customer_display_currency_char
-            }
+            };
 
             this.proxy.send_currency_data_customer_display(currency_data);
         }
     });
 
-
     devices.ProxyDevice = devices.ProxyDevice.extend({
         send_currency_data_customer_display: function (currency_data) {
             return this.message('send_currency_data_customer_display', {'currency_data': JSON.stringify(currency_data)});
         },
+    });
+
+    var OrderlineSuper = models.Orderline;
+
+    models.Orderline = models.Orderline.extend({
+        get_quantity_str_with_unit: function(){
+            var unit = this.get_unit();
+            console.log(unit);
+            if(unit && !unit.is_pos_groupable){
+                return this.quantityStr + ' ' + unit.name;
+            }else{
+                return round_pr(this.quantityStr, 0.1);
+            }
+        },
+        // only for bixolon display
+        get_unit_price_with_unit: function(currency_char){
+            var unit = this.get_unit();
+            if(unit && !unit.is_pos_groupable){
+                return this.get_unit_price() + currency_char + '/' + unit.name;
+            }else{
+                return this.get_unit_price()  + currency_char;
+            }
+        },
+
     });
 
     // TODO(Vincent) is it the best place to fetch and set currency data?
