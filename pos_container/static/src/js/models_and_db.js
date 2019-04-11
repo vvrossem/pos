dc2f75b148efd30447def63032ee7ccad4a058b3
Pierrick Brun <pierrick.brun@akretion.com>
2019-04-11 18:00:22 +0200

    [FIX] correctly display XmlReceipt

--- pos_container/static/src/js/models_and_db.js
+++ pos_container/static/src/js/models_and_db.js
@@ -94,6 +94,111 @@ odoo.define('pos_container.models_and_db', function (require) {
             }
             return false;
         },
+        export_for_printing: function(){
+            var orderlines = [];
+            var self = this;
+
+            this.orderlines.each(function(orderline){
+                orderlines.push(orderline.export_for_printing());
+            });
+
+            var paymentlines = [];
+            this.paymentlines.each(function(paymentline){
+                paymentlines.push(paymentline.export_for_printing());
+            });
+            var client  = this.get('client');
+            var cashier = this.pos.get_cashier();
+            var company = this.pos.company;
+            var shop    = this.pos.shop;
+            var date    = new Date();
+
+            function is_xml(subreceipt){
+                return subreceipt ? (subreceipt.split('\n')[0].indexOf('<!DOCTYPE QWEB') >= 0) : false;
+            }
+
+            function render_xml(subreceipt){
+                if (!is_xml(subreceipt)) {
+                    return subreceipt;
+                } else {
+                    subreceipt = subreceipt.split('\n').slice(1).join('\n');
+                    var qweb = new QWeb2.Engine();
+                        qweb.debug = config.debug;
+                        qweb.default_dict = _.clone(QWeb.default_dict);
+                        qweb.add_template('<templates><t t-name="subreceipt">'+subreceipt+'</t></templates>');
+
+                    return qweb.render('subreceipt',{'pos':self.pos,'widget':self.pos.chrome,'order':self, 'receipt': receipt}) ;
+                }
+            }
+
+            var receipt = {
+                orderlines: orderlines,
+                paymentlines: paymentlines,
+                subtotal: this.get_subtotal(),
+                total_with_tax: this.get_total_with_tax(),
+                total_without_tax: this.get_total_without_tax(),
+                total_tax: this.get_total_tax(),
+                total_paid: this.get_total_paid(),
+                total_discount: this.get_total_discount(),
+                tax_details: this.get_tax_details(),
+                change: this.get_change(),
+                name : this.get_name(),
+                client: client ? client.name : null ,
+                invoice_id: null,   //TODO
+                cashier: cashier ? cashier.name : null,
+                precision: {
+                    price: 2,
+                    money: 2,
+                    quantity: 3,
+                },
+                date: {
+                    year: date.getFullYear(),
+                    month: date.getMonth(),
+                    date: date.getDate(),       // day of the month
+                    day: date.getDay(),         // day of the week
+                    hour: date.getHours(),
+                    minute: date.getMinutes() ,
+                    isostring: date.toISOString(),
+                    localestring: date.toLocaleString(),
+                },
+                company:{
+                    email: company.email,
+                    website: company.website,
+                    company_registry: company.company_registry,
+                    contact_address: company.partner_id[1],
+                    vat: company.vat,
+                    vat_label: company.country && company.country.vat_label || '',
+                    name: company.name,
+                    phone: company.phone,
+                    logo:  this.pos.company_logo_base64,
+                },
+                shop:{
+                    name: shop.name,
+                },
+                currency: this.pos.currency,
+                //custom here
+                has_tare_mode: {
+                    auto: this.has_tare_line('AUTO'),
+                    manual: this.has_tare_line('MAN'),
+                }
+                //custom end
+            };
+
+            if (is_xml(this.pos.config.receipt_header)){
+                receipt.header = '';
+                receipt.header_xml = render_xml(this.pos.config.receipt_header);
+            } else {
+                receipt.header = this.pos.config.receipt_header || '';
+            }
+
+            if (is_xml(this.pos.config.receipt_footer)){
+                receipt.footer = '';
+                receipt.footer_xml = render_xml(this.pos.config.receipt_footer);
+            } else {
+                receipt.footer = this.pos.config.receipt_footer || '';
+            }
+
+            return receipt;
+        },
     });
     
     // Add container to order line
@@ -139,6 +244,28 @@ odoo.define('pos_container.models_and_db', function (require) {
         set_gross_weight: function(weight){
             this.gross_weight = weight;
             this.trigger('change', this);
+        },
+        //used to create a json of the ticket, to be sent to the printer
+        export_for_printing: function(){
+            return {
+                quantity:           this.get_quantity(),
+                unit_name:          this.get_unit().name,
+                price:              this.get_unit_price(),
+                discount:           this.get_discount(),
+                product_name:       this.get_product().display_name,
+                product_name_wrapped: this.generate_wrapped_product_name(),
+                price_display :     this.get_display_price(),
+                price_with_tax :    this.get_price_with_tax(),
+                price_without_tax:  this.get_price_without_tax(),
+                tax:                this.get_tax(),
+                product_description:      this.get_product().description,
+                product_description_sale: this.get_product().description_sale,
+                // extension starts here
+                container:          this.get_container(),
+                tare:               this.get_tare(),
+                tare_mode:          this.get_tare_mode(),
+                gross_weight:       this.get_gross_weight(),
+            };
         },
     });
 
